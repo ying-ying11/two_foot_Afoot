f98bf274cd73e1b52f963e6b68b4f906f1f8647d
EternalFlyn <EternalFlyn@gmail.com>
2022-11-10 15:24:58 +0800

    [0.2.4] - 2022-11-08 added - encrypt file when file upload

--- app/src/main/java/com/flyn/sarcopenia_project/net/Client.kt
+++ app/src/main/java/com/flyn/sarcopenia_project/net/Client.kt
@@ -1,19 +1,43 @@
 package com.flyn.sarcopenia_project.net
 
 import android.content.Context
-import com.flyn.fc_message.base.RawMessageEncoder
+import com.flyn.fc_message.base.RawMessageCodec
+import com.flyn.fc_message.secure.AesCodec
+import com.flyn.fc_message.secure.RsaCodec
+import com.flyn.fc_message.secure.decodeHex
+import io.github.cdimascio.dotenv.dotenv
 import io.netty.bootstrap.Bootstrap
 import io.netty.channel.ChannelInitializer
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioSocketChannel
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder
+import io.netty.handler.codec.LengthFieldPrepender
 import java.io.File
 import java.net.InetSocketAddress
+import java.security.KeyFactory
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.X509EncodedKeySpec
 import java.util.*
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 object Client {
 
-    private val uuid = UUID.randomUUID()
+    private val publicKey: RSAPublicKey
+    private val aesKeySpec: SecretKeySpec
+    internal val ivSpec: IvParameterSpec
+
+    init {
+        val dotenv = dotenv {
+            directory = "/assets"
+            filename = "env"
+        }
+        publicKey = KeyFactory.getInstance("RSA")
+            .generatePublic(X509EncodedKeySpec(dotenv["PUBLIC_KEY"]?.decodeHex())) as RSAPublicKey
+        aesKeySpec = SecretKeySpec(dotenv["SECRET_KEY"]?.decodeHex(), "AES")
+        ivSpec = IvParameterSpec(dotenv["IV_PARAMETER"]?.decodeHex())
+    }
 
     fun transferFiles(context: Context, files: Array<File>, host: String = "localhost", port: Int = 8787) {
         val workerGroup = NioEventLoopGroup()
@@ -25,7 +49,14 @@ object Client {
                 handler(object: ChannelInitializer<NioSocketChannel>() {
 
                     override fun initChannel(ch: NioSocketChannel) {
-                        ch.pipeline().addLast(RawMessageEncoder(), ClientHandler(context, uuid, files))
+                        with (ch.pipeline()) {
+                            addLast(LengthFieldBasedFrameDecoder(10240, 0, 2, 0, 2))
+                            addLast(LengthFieldPrepender(2))
+                            addLast(AesCodec(aesKeySpec, ivSpec))
+                            addLast(RawMessageCodec())
+                            addLast(RsaCodec(publicKey = publicKey))
+                            addLast(ClientHandler(context, files))
+                        }
                     }
 
                 })
--- app/src/main/java/com/flyn/sarcopenia_project/net/ClientHandler.kt
+++ app/src/main/java/com/flyn/sarcopenia_project/net/ClientHandler.kt
@@ -3,6 +3,7 @@ package com.flyn.sarcopenia_project.net
 import android.content.Context
 import android.widget.Toast
 import com.flyn.fc_message.message.FileMessage
+import com.flyn.fc_message.message.KeyMessage
 import com.flyn.fc_message.message.UUIDMessage
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
@@ -14,24 +15,44 @@ import java.io.IOException
 import java.io.RandomAccessFile
 import java.nio.ByteBuffer
 import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
 
-class ClientHandler(private val context: Context, private val uuid: UUID, private val files: Array<File>): ChannelInboundHandlerAdapter() {
+class ClientHandler(private val context: Context, private val files: Array<File>): ChannelInboundHandlerAdapter() {
 
     companion object {
         private const val bufferSize = 1024
+        private val uuid = UUID.randomUUID()
+        private val cipher = Cipher.getInstance("AES/CTR/NoPadding")
+        private val keyGen = KeyGenerator.getInstance("AES")
+
+        init {
+            keyGen.init(KeyMessage.KEY_LENGTH)
+        }
     }
+
+    private val buffer = ByteBuffer.allocateDirect(bufferSize)
+    private val encryptBuffer = ByteBuffer.allocateDirect(bufferSize)
 
     override fun channelActive(ctx: ChannelHandlerContext) {
         ctx.writeAndFlush(UUIDMessage.encoder(ctx, UUIDMessage(uuid)))
         files.forEachIndexed { index, file ->
+            val key = keyGen.generateKey()
+            cipher.init(Cipher.ENCRYPT_MODE, key, Client.ivSpec)
+            ctx.writeAndFlush(KeyMessage.encoder(ctx, KeyMessage(key)))
             try {
                 val channel = RandomAccessFile(file, "r").channel
                 var isRemaining = true
                 while (isRemaining) {
-                    val buffer = ByteBuffer.allocateDirect(bufferSize)
                     isRemaining = channel.read(buffer) == bufferSize
                     buffer.flip()
-                    FileMessage(file.name, isRemaining, buffer).run {
+
+                    // encrypt
+                    if (isRemaining) cipher.update(buffer, encryptBuffer)
+                    else cipher.doFinal(buffer, encryptBuffer)
+                    encryptBuffer.flip()
+
+                    FileMessage(file.name, isRemaining, encryptBuffer).run {
                         ctx.writeAndFlush(FileMessage.encoder(ctx, this)).let {
                             if (!isRemaining && index == files.size - 1) {
                                 it.addListener {
@@ -43,6 +64,9 @@ class ClientHandler(private val context: Context, private val uuid: UUID, privat
                             }
                         }
                     }
+
+                    buffer.clear()
+                    encryptBuffer.clear()
                 }
             } catch (exception: IOException) {
                 exception.printStackTrace()
